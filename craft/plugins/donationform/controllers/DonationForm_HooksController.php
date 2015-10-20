2559f5a4e58940e98074ada6562e069af4211049
Jan Rubio <rubiojan@gmail.com>
2015-10-19 23:05:18 -0400

    Preparing for deploy

--- craft/plugins/donationform/controllers/DonationForm_HooksController.php
+++ craft/plugins/donationform/controllers/DonationForm_HooksController.php
@@ -29,7 +29,7 @@ class DonationForm_HooksController extends BaseController
     {
         $mandrill = craft()->mandrillService_wrapper->requireApi();
 
-        $cancelURL = 'http://localhost:8888/donation/cancel?a='.$json->data->object->customer;
+        $cancelURL = 'https://stapinc.org/donation/cancel?a='.$json->data->object->customer;
         $amount = $json->data->object->amount_due;
         $bodyOne = craft()->donationForm_emailSettings->getOrCreateEmailSetting()->receiptParagraphOne;
         $bodyTwo = craft()->donationForm_emailSettings->getOrCreateEmailSetting()->receiptParagraphTwo;
